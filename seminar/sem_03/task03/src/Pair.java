a0f0c5eaf331694d59f466714d97ca4aabcbfaa2
Yuriy Gelvih <Gelvihurij@gmail.com>
2024-02-21 16:01:13 +0700

    Adding practical work on the topic "generalized programming"
    #practical assignments for the lecture
    #Practical tasks for the seminar

--- seminar/sem_03/task03/src/Pair.java
+++ seminar/sem_03/task03/src/Pair.java
@@ -1,6 +1,6 @@
 public class Pair<T1, T2> {
-    private final T1 first;
-    private final T2 second;
+    private T1 first;
+    private T2 second;
 
     public Pair(T1 first, T2 second) {
         this.first = first;
