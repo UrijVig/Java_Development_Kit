e5e176ac52c90e1929ef3f0557987d73b4789568
Work VM <Gelvihurij@gmail.com>
2024-02-23 02:55:22 +0700

    edits

--- seminar/sem_04/src/main/java/app/Employee.java
+++ seminar/sem_04/src/main/java/app/Employee.java
@@ -1,13 +1,4 @@
 package main.java.app;
 
 public record Employee(int personalNumber, String phoneNumber, String name, int experience) {
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "personalNumber=" + personalNumber +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", name='" + name + '\'' +
-                ", experience=" + experience +
-                '}';
-    }
 }
